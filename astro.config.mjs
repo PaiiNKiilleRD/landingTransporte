cd4cb8b48dbfe16f284c4869fc86ad8ea445b145
PaiiNKiilleRD <javiervitalioperalta@gmail.com>
2024-06-16 20:33:26 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  // site: 'https://PaiiNKiilleRD.github.io',
+  site: 'https://PaiiNKiilleRD.github.io',
   base: '/transporteLanding',
 });
