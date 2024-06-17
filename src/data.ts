cd2352b494e33dd1445a5b282331f0f77724c0fa
PaiiNKiilleRD <javiervitalioperalta@gmail.com>
2024-06-16 20:31:39 -0400

    links fixed

--- src/data.ts
+++ src/data.ts
@@ -1,18 +1,18 @@
 export const links = [
   {
     text: "Inicio",
-    path: "/",
+    path: "/transporteLanding",
   },
   {
     text: "Servicios",
-    path: "/servicios",
+    path: "/transporteLanding/servicios",
   },
   {
     text: "Sobre Nosotros",
-    path: "/",
+    path: "/transporteLanding/servicios",
   },
   {
     text: "Contactanos",
-    path: "/",
+    path: "/transporteLanding/servicios",
   },
 ];
